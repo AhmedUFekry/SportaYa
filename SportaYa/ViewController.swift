789f6583c02155d61e505aa56b970e2d9ccb09e7
A7med Fekry <a7medfekry@A7meds-MacBook-Pro.local>
2024-04-25 01:42:24 -0700

    creating the project

---
+++ SportaYa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportaYa
+//
+//  Created by A7med Fekry on 25/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
